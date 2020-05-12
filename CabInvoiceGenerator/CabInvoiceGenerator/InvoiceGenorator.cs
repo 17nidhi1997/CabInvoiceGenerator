1bf37eb1dec20a468202d4fd6028b3eedd5d0e01
17nidhi1997 <nidhinavaya1997@gmail.com>
2020-05-12 12:59:03 +0530

    Added : Mutiple Rides Should return total Fare

---
+++ CabInvoiceGenerator/CabInvoiceGenerator/InvoiceGenorator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CabInvoiceGenerator
+{
+    class InvoiceGenorator
+    {
+        private const double minimumCostPerKilometer = 10;
+        private const int costPerTime = 1;
+        private const int mimumFare = 5;
+
+
+        public double CalculateFare(double distance, int time)
+        {
+
+            double totalFare = distance * minimumCostPerKilometer + time * costPerTime;
+            if (totalFare < mimumFare)
+                return mimumFare;
+            else
+                return totalFare;
+        }
+        public double CalculateFare(Ride[] rides)
+        {
+            double totalFare = 0;
+
+            foreach (Ride ride in rides)
+            {
+                totalFare += this.CalculateFare(ride.distance, ride.time);
+            }
+            return totalFare;
+        }
+    }
---
+++ CabInvoiceGenerator/CabInvoiceGenerator/UnitTest1.cs
@@ -0,0 +1,48 @@
+using NUnit.Framework;
+
+namespace CabInvoiceGenerator
+{
+    public class Tests
+    {
+        /// <summary>
+        /// Test 1.1
+        /// </summary>
+        [Test]
+        public void GivenLessDistanceAndTime_ShouldReturnTotalFare()
+        {
+            InvoiceGenorator invoice = new InvoiceGenorator();
+            double distance = 2.0;
+            int time = 5;
+            double fare = invoice.CalculateFare(distance, time);
+            Assert.AreEqual(expected: 25, fare, delta: 0.0);
+        }
+
+        /// <summary>
+        /// Test 1.2
+        /// </summary>
+        [Test]
+        public void GivenLessDistanceAndTime_ShouldReturnMinFare()
+        {
+            InvoiceGenorator invoice = new InvoiceGenorator();
+            double distance = 0.1;
+            int time = 1;
+            double fare = invoice.CalculateFare(distance, time);
+            Assert.AreEqual(expected: 5, fare, delta: 0.0);
+        }
+
+        /// <summary>
+        /// Test 2
+        /// </summary>
+        [Test]
+        public void GivenMultipleRider_ShouldReturnTotalFare()
+        {
+            InvoiceGenorator invoice = new InvoiceGenorator();
+            Ride[] rides = { new Ride(distance: 2.0 ,time : 5),
+                            new Ride ( distance:0.1,time :1)
+            };
+
+            double fare = invoice.CalculateFare(rides);
+            Assert.AreEqual(expected: 30, fare, delta: 0.0);
+        }
+    }
+}
