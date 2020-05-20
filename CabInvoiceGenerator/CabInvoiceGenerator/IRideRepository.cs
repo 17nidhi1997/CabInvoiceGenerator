beef75b99c3cacefd5375b59d8d7b3eb999edad2
17nidhi1997 <nidhinavaya1997@gmail.com>
2020-05-20 10:30:13 +0530

    [Srinidhi]Refactor: refactor the code to make a code depeandancy injuction

---
+++ CabInvoiceGenerator/CabInvoiceGenerator/IRideRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cabInvoiceGenerator
+{
+    public interface IRideRepository
+    {
+        public void AddRides(string userId, Ride[] rides);
+        public Ride[] GetRides(string userId);
+    }
+}
