d961650a9eaac543e75482c1149373c7f3748cb2
17nidhi1997 <nidhinavaya1997@gmail.com>
2020-05-20 09:12:20 +0530

    [Srinidhi]Refactor: refactor the code to add Enum

---
+++ CabInvoiceGenerator/CabInvoiceGenerator/cabRide.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cabInvoiceGenerator
+{
+   public class cabRide
+    {
+        public static readonly cabRide Normal = new cabRide(10.0, 1.0, 5.0);
+        public static readonly cabRide Premium = new cabRide(15.0, 2.0, 20.0);
+        public static IEnumerable<cabRide> Values
+        {
+            get
+            {
+                yield return Normal;
+                yield return Premium;
+            }
+        }
+        public double CostPerKilometer { get; private set; }
+        public double CostPerMin { get; private set; }
+        public double MinFare { get; private set; }
+        public cabRide(double CostPerKilometer, double CostPerMin, double MinFare)
+        {
+            this.CostPerKilometer = CostPerKilometer;
+            this.CostPerMin = CostPerMin;
+            this.MinFare = MinFare;
+        }
+
+        public double CalculateFare(Ride ride)
+        {
+            double RideCost = ride.distance * CostPerKilometer + ride.time * CostPerMin;
+            return Math.Max(RideCost, MinFare);
+        }
+    }
+}
