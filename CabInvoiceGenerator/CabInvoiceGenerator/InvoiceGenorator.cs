a13c750fe249993d174dd17141aed0884f65e15b
17nidhi1997 <nidhinavaya1997@gmail.com>
2020-05-20 09:14:04 +0530

    [Srinidhi]Refactor: refactor the code to add Enum

--- CabInvoiceGenerator/CabInvoiceGenerator/InvoiceGenorator.cs
+++ CabInvoiceGenerator/CabInvoiceGenerator/InvoiceGenorator.cs
@@ -2,51 +2,32 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace CabInvoiceGenerator
+namespace cabInvoiceGenerator
 {
     class InvoiceGenorator
     {
-        public const double minimumCostPerKilometer = 10.0;
-        public const int costPerTime = 1;
-        public const int mimumFare = 5;
-        // Declared and Initialised Variables of Premimum rides
-        readonly private double premiusCostPerKiloMeter = 15.0;
-        readonly private double premimunCostPerMinute = 2.0;
-        readonly private double premimunMinimumFare = 20.0;
+        public  double minimumCostPerKilometer = 10.0;
+        public   int costPerTime = 1;
+        public  int mimumFare = 5;
 
 
-        public double CalculateFare(string rideType, double Distance, double Time)
+        public double CalculateFare(cabRide cabride, double Distance, double Time)
         {
-            if (rideType == "normal")
-            {
-                double totalFare = Distance * minimumCostPerKilometer + Time * costPerTime;
-                if (totalFare < mimumFare)
-                    return mimumFare;
-                else
-                    return totalFare;
-            }
-            // Calculation for premimunFare
-            if (rideType == "premium")
-            {
-                double totalFare = (Distance * premiusCostPerKiloMeter) + (Time * premimunCostPerMinute);
-                if (totalFare > premimunMinimumFare)
-                {
-                    return totalFare;
-                }
-            }
-            return premimunMinimumFare;
+
+            double totalFare = Distance * minimumCostPerKilometer + Time * costPerTime;
+            if (totalFare < mimumFare)
+                return mimumFare;
+            else
+                return totalFare;
         }
-
-
-
         public InvoiceSummary CalculateFare(Ride[] rides)
         {
             double totalFare = 0;
             int noOfRides = 0;
             foreach (Ride ride in rides)
             {
-                totalFare += CalculateFare(ride.rideType, ride.distance, ride.time);
-                noOfRides += 1;
+                totalFare += CalculateFare(ride.rideType, ride.distance,ride.time);
+                noOfRides++;
             }
             InvoiceSummary invoiceSummary = new InvoiceSummary();
             invoiceSummary.noOfRides = noOfRides;
