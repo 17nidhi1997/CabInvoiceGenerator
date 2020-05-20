414ab01fbbbc3cfbbcf4da38946b5edfb0b088ca
17nidhi1997 <nidhinavaya1997@gmail.com>
2020-05-20 09:14:32 +0530

    [Srinidhi]Refactor: refactor the code to add Enum

--- CabInvoiceGenerator/CabInvoiceGenerator/InvoiceSummary.cs
+++ CabInvoiceGenerator/CabInvoiceGenerator/InvoiceSummary.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace CabInvoiceGenerator
+namespace cabInvoiceGenerator
 {
     public class InvoiceSummary
     {
@@ -16,4 +16,4 @@ namespace CabInvoiceGenerator
         }
 
     }
-}
+}      
